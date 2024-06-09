a89fde179875a4d712f45a026969e1ab0e71e74a
ackwell <saxon@ackwell.com.au>
2024-06-09 20:41:41 +1000

    Set up base alias handling

--- src/search/sqlite/query.rs
+++ src/search/sqlite/query.rs
@@ -1,10 +1,12 @@
+use std::collections::HashSet;
+
 use ironworks::{
 	excel::{Field, Language, Row, Sheet},
 	file::exh,
 };
 use sea_query::{
 	Alias, ColumnDef, ColumnType, Condition, DynIden, Expr, Iden, InsertStatement, IntoCondition,
-	Query, SelectStatement, SimpleExpr, Table, TableCreateStatement, TableDropStatement,
+	Query, SelectStatement, SimpleExpr, Table, TableCreateStatement, TableDropStatement, TableRef,
 };
 
 use crate::{
@@ -25,7 +27,7 @@ pub fn table_create(sheet: &Sheet<String>, language: Language) -> Result<TableCr
 	// confer aren't particularly meaningful for our workload.
 	let mut table = Table::create();
 	table
-		.table(table_name(sheet, language))
+		.table(table_name(&sheet.name(), language))
 		.col(ColumnDef::new(KnownColumn::RowId).integer().primary_key());
 
 	if matches!(kind, exh::SheetKind::Subrows) {
@@ -44,7 +46,7 @@ pub fn table_create(sheet: &Sheet<String>, language: Language) -> Result<TableCr
 
 pub fn table_drop(sheet: &Sheet<String>, language: Language) -> TableDropStatement {
 	Table::drop()
-		.table(table_name(&sheet, language))
+		.table(table_name(&sheet.name(), language))
 		.if_exists()
 		.take()
 }
@@ -63,16 +65,16 @@ pub fn table_insert(sheet: &Sheet<String>, language: Language) -> Result<InsertS
 	}
 
 	let statement = Query::insert()
-		.into_table(table_name(sheet, language))
+		.into_table(table_name(&sheet.name(), language))
 		.columns(columns)
 		.to_owned();
 
 	Ok(statement)
 }
 
-fn table_name(sheet: &Sheet<String>, language: Language) -> Alias {
+fn table_name(sheet_name: &str, language: Language) -> Alias {
 	let language_string = LanguageString::from(language);
-	Alias::new(format!("sheet-{}@{language_string}", sheet.name()))
+	Alias::new(format!("sheet-{sheet_name}@{language_string}"))
 }
 
 // TODO: update IW to return an iterator over col defs so this cols param isn't required for shared access
@@ -168,43 +170,91 @@ fn field_value(field: Field) -> SimpleExpr {
 
 pub fn resolve_queries(queries: Vec<(String, post::Node)>) -> SelectStatement {
 	// temp. obviously.
-	let q1 = queries.into_iter().next().unwrap();
-	resolve_query(q1.1)
+	let (sheet_name, node) = queries.into_iter().next().unwrap();
+	resolve_query(sheet_name, node)
 }
 
-fn resolve_query(node: post::Node) -> SelectStatement {
-	let mut query = Query::select();
-	query.from(Alias::new("BOGUSTEST"));
+fn resolve_query(sheet_name: String, node: post::Node) -> SelectStatement {
+	let alias = "alias-0".to_string();
 
-	let condition = resolve_node(node);
+	let ResolveResult {
+		condition,
+		languages,
+	} = resolve_node(
+		node,
+		&ResolveContext {
+			alias: alias.clone(),
+		},
+	);
+
+	let mut query = Query::select();
+
+	// TODO: this will probably need to be split out for reuse at relation boundaries
+	let mut table_references = languages.into_iter().map(|language| {
+		let alias = table_alias(&alias, language);
+		(
+			alias.clone(),
+			TableRef::TableAlias(
+				DynIden::new(table_name(&sheet_name, language)),
+				DynIden::new(alias),
+			),
+		)
+	});
+
+	// TODO: is it possible for there to be no languages and hence no joins? is that a failure? what about on relation boundaries
+	let (base_alias, base_reference) = table_references.next().expect("TODO: handle");
+	query.from(base_reference);
+	for (join_alias, join_reference) in table_references {
+		query.inner_join(
+			join_reference,
+			Expr::col((join_alias, KnownColumn::RowId))
+				.equals((base_alias.clone(), KnownColumn::RowId)),
+		);
+	}
+
 	query.cond_where(condition);
 
 	query.take()
 }
 
-fn resolve_node(node: post::Node) -> Condition {
-	// Query::select().cond_where(condition)
-	// Condition::;
+struct ResolveContext {
+	alias: String,
+}
+
+struct ResolveResult {
+	condition: Condition,
+	languages: HashSet<Language>,
+}
+
+fn resolve_node(node: post::Node, context: &ResolveContext) -> ResolveResult {
 	match node {
-		post::Node::Group(group) => resolve_group(group),
-		post::Node::Leaf(leaf) => resolve_leaf(leaf),
+		post::Node::Group(group) => resolve_group(group, context),
+		post::Node::Leaf(leaf) => resolve_leaf(leaf, context),
 	}
 }
 
-fn resolve_group(group: post::Group) -> Condition {
+fn resolve_group(group: post::Group, context: &ResolveContext) -> ResolveResult {
 	// for a given group, MUST are top-level AND, SHOULD are "AND (A OR B OR C...)", and MUSTNOT are AND NOT
 	// given that, the root of a group is all ANDs, so we can use ::all and collect ORs for the SHOULD
 	let mut must = Condition::all();
 	let mut should = Condition::any();
 	let mut must_not = Condition::any().not();
 
+	let mut languages = HashSet::new();
+
 	for (occur, node) in group.clauses {
-		let inner_condition = resolve_node(node);
+		let ResolveResult {
+			condition: inner_condition,
+			languages: inner_languages,
+		} = resolve_node(node, context);
+
 		match occur {
 			post::Occur::Must => must = must.add(inner_condition),
 			post::Occur::Should => should = should.add(inner_condition),
 			post::Occur::MustNot => must_not = must_not.add(inner_condition),
 		}
+
+		languages.extend(inner_languages)
 	}
 
 	// NOTE: we're only adding if c.len=0 here because any number of SHOULDs do not effect the _filtering_ of a query if there's 1 or more MUSTs - only the scoring. which i don't have any idea how to do. well, that's a lie. but still.
@@ -219,20 +269,32 @@ fn resolve_group(group: post::Group) -> Condition {
 	// TODO: this would need to record a condition for scoring as well
 	// realistically; MUSTs in queries will always match, so a scoring structure only needs to account for the SHOULDs as actual conditions, and can pass up a static integer of the number of MUSTs that can be added to the score
 
-	must
+	ResolveResult {
+		condition: must,
+		languages,
+	}
 }
 
-fn resolve_leaf(leaf: post::Leaf) -> Condition {
-	// TODO: this is obviously bollocks - needs a table/alias reference, and handle language
+fn resolve_leaf(leaf: post::Leaf, context: &ResolveContext) -> ResolveResult {
 	let (column_definition, language) = leaf.field;
-	let expression = Expr::col(column_name(&column_definition));
+	let expression = Expr::col((
+		table_alias(&context.alias, language),
+		column_name(&column_definition),
+	));
 
-	let se = match leaf.operation {
+	let resolved_expression = match leaf.operation {
 		post::Operation::Relation(relation) => todo!(),
 		// TODO: need to handle escaping
 		post::Operation::Match(string) => expression.like(format!("%{string}%")),
 		post::Operation::Equal(value) => todo!(),
 	};
 
-	se.into_condition()
+	ResolveResult {
+		condition: resolved_expression.into_condition(),
+		languages: HashSet::from([language]),
+	}
+}
+
+fn table_alias(alias_base: &str, language: Language) -> Alias {
+	Alias::new(format!("{alias_base}@{}", LanguageString::from(language)))
 }
