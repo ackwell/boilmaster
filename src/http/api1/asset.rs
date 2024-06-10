2074fdfa3e972be8deeeb4eb070662627b77cf35
ackwell <saxon@ackwell.com.au>
2024-06-10 19:58:21 +1000

    Add ETag check to asset endpoint

--- src/http/api1/asset.rs
+++ src/http/api1/asset.rs
@@ -1,12 +1,25 @@
-use std::ffi::OsStr;
+use std::{
+	ffi::OsStr,
+	hash::{Hash, Hasher},
+};
 
 use axum::{
-	debug_handler, extract::State, http::header, response::IntoResponse, routing::get, Router,
+	debug_handler,
+	extract::State,
+	http::header,
+	response::{IntoResponse, Response},
+	routing::get,
+	Router,
 };
-use axum_extra::{headers::ContentType, TypedHeader};
+use axum_extra::{
+	headers::{ContentType, ETag, IfNoneMatch},
+	TypedHeader,
+};
+use reqwest::StatusCode;
+use seahash::SeaHasher;
 use serde::Deserialize;
 
-use crate::{asset::Format, http::service};
+use crate::{asset::Format, http::service, version::VersionKey};
 
 use super::{
 	error::Result,
@@ -27,9 +40,18 @@ async fn asset(
 	Path(path): Path<String>,
 	VersionQuery(version_key): VersionQuery,
 	Query(query): Query<AssetQuery>,
+	header_if_none_match: Option<TypedHeader<IfNoneMatch>>,
 	State(asset): State<service::Asset>,
-) -> Result<impl IntoResponse> {
+) -> Result<Response> {
 	let format = query.format;
+
+	let etag = etag(&path, format, version_key);
+
+	if let Some(TypedHeader(if_none_match)) = header_if_none_match {
+		if !if_none_match.precondition_passes(&etag) {
+			return Ok(StatusCode::NOT_MODIFIED.into_response());
+		}
+	}
 
 	let bytes = asset.convert(version_key, &path, format)?;
 
@@ -43,12 +65,25 @@ async fn asset(
 		TypedHeader(ContentType::from(format_mime(format))),
 		// TypedHeader only has a really naive inline value with no ability to customise :/
 		[(header::CONTENT_DISPOSITION, disposition)],
+		TypedHeader(etag),
 		bytes,
-	))
+	)
+		.into_response())
 }
 
 fn format_mime(format: Format) -> mime::Mime {
 	match format {
 		Format::Png => mime::IMAGE_PNG,
 	}
+}
+
+fn etag(path: &str, format: Format, version: VersionKey) -> ETag {
+	let mut hasher = SeaHasher::new();
+	path.hash(&mut hasher);
+	format.extension().hash(&mut hasher);
+	let resource_hash = hasher.finish();
+
+	format!("\"{resource_hash:016x}.{version}\"")
+		.parse()
+		.expect("malformed etag")
 }
